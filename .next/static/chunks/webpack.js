14b04fe94714c3ac7ac414131974257fe6efa74a
guilhermeabel99 <gpeuvis@gmail.com>
2020-12-30 22:52:45 -0300

    feature branch

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "8d254738ed13e859d6a5";
+/******/ 	var hotCurrentHash = "3d5de4c3a502f4fd6c9b";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
